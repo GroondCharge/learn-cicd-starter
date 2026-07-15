080c8ddc7b079ff69676b5ddfc344d056b586b5f
janz <jan.zalar6@gmail.com>
2026-07-15 08:21:02 +0200

    rm faulty func

--- main.go
+++ main.go
@@ -24,12 +24,7 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
-func unused() {
-    // this function does nothing
-    // and is called nowhere
-}
-
-func main(){
+func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
